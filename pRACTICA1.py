9939dec6beac2dd0892d99bbe9bf38d8333a63a0
ToledoAgustin <Agustin.benitez@sagradoalcal.edu.ar>
2025-10-22 15:00:42 -0300

    Subiendo mi appTK

--- pRACTICA1.py
+++ pRACTICA1.py
@@ -7,20 +7,22 @@ ventana.geometry("300x500")
 ventana.resizable(False,False)
 
 
+frame1 = tk.Frame(ventana, bg="pink", width=200, height=300,bd = 5)
+frame1.pack(pady=20)
+
 
 usuario_correcto = "Mesi"
 clave_correcta = "holasoymessi12345"
 
 
-etiqueta_usuario = tk.Label(ventana, text="usuario:")
-etiqueta_usuario.pack()
-ingreso_usuario = tk.Entry(ventana)
+etiqueta_usuario = tk.Label(frame1, text="usuario:",bg="pink")
+etiqueta_usuario.pack(pady=10)
+ingreso_usuario = tk.Entry(frame1)
 ingreso_usuario.pack()
 
-
-etiqueta_contraseña = tk.Label(ventana, text="contraseña:")
+etiqueta_contraseña = tk.Label(frame1, text="contraseña:",bg="pink")
 etiqueta_contraseña.pack()
-ingreso_contraseña = tk.Entry(ventana)
+ingreso_contraseña = tk.Entry(frame1)
 ingreso_contraseña.pack()
 
 
@@ -28,9 +30,9 @@ def iniciar_sesion():
     Usuario = ingreso_usuario.get()
     contrasenia = ingreso_contraseña.get()
     if Usuario == usuario_correcto and contrasenia == clave_correcta:
-        etiqueta_resultado.config(text="Inicio sesion exitosamente.",fg= "green")
+        etiqueta_resultado.config(text="!Inicio sesion exitosamente¡.",fg= "green")
     else:
-        etiqueta_resultado.config(text="usuario o contraseña incorrectos.", fg="red")
+        etiqueta_resultado.config(text="usuario o contraseña incorrectos!.", fg="red")
 
 
 boton = tk.Button(ventana, text = "iniciar sesion", bg = "sky blue", command = iniciar_sesion)
